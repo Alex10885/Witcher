b26680546c5a7457ea3fb48cd07fb672f2a06f40
Alexey Doronin <aledor10885@gmail.com>
2020-02-06 20:10:14 +0300

    добавлен адаптив

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-const mySwiper = new Swiper('.swiper-container', {
-    slidesPerView: 2,
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
     loop: true,
     navigation: {
         nextEl: '.arrow'
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        }
     }
+});
+
+var menuButton = document.querySelector('.menu__button');
+var menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', function () {
+    menuButton.classList.toggle('menu__button_active');
+    menu.classList.toggle('header__active');
 });
